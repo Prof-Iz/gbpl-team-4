606e016e96e78a81085cbc012540213b46c85bad
Izdhan <57706976+Prof-Iz@users.noreply.github.com>
2021-02-24 23:04:27 +0800

    Finally working i guess?

--- IZ/dash_test.py
+++ IZ/dash_test.py
@@ -4,6 +4,9 @@ import dash_html_components as html
 import pandas as pd
 import json
 import numpy as np
+from dash.dependencies import Output, Input
+import plotly.express as px
+
 
 # server_url = ''
 # request=Request(server_url)
@@ -13,7 +16,6 @@ import numpy as np
 # df = pd.json_normalize(data['results'])
 
 data = pd.read_csv(r"C:\Users\User\Desktop\GBPL\gbpl-team-4\IZ\test.csv")
-print(data)
 
 
 external_stylesheets = [
@@ -30,45 +32,63 @@ app.title = "GPBL 04"
 app.layout = html.Div(
     children=[
         html.Div(
-        children = [html.H1(children="💦 🔥 😨",className="header-emoji"),
-        html.H1("Group 4 GPBL",className="header-title"),
-        html.P(
-            children="Test Dash Application for our Stuff",
-            className="header-description"
-        ),],
-        className="header"),
-        html.Div(
-            children = dcc.Graph(
-                figure={
-                    "data": [
-                        {
-                            "x": np.arange(1,data["temp"].count()),
-                            "y": data["temp"],
-                            "type": "lines",
-                            "hovertemplate": "%{y:.2f} C",
-                        },
+            children=[html.H1(children="💦 🔥 😨", className="header-emoji"),
+                      html.H1("Group 4 GPBL", className="header-title"),
+                      html.P(
+                children=[],
+                id="test_stuff",
+
+                className="header-description"
+            ), ],
+            className="header"),
+        html.Div(children=[
+            html.Div(children=[
+                html.Div(children="Type", className="menu-title"),
+                dcc.Dropdown(
+                    id="type-filter",
+                    options=[
+                        {"label": "Temperature", "value": "temp"},
+                        {"label": "Humidity", "value": "humidity"},
                     ],
-                    "layout": {"title": "Temperature over time",
-                    "colorway": ["#17B897"]},
-                },
-            ),
-            className="card"),
+                    value="temp",
+                    clearable=False,
+                    className="dropdown"
+                ),
+            ],),
+            html.Div(children=[
+                html.Div(children="How Many Records", className="menu-title"),
+                dcc.Slider(
+                    id="records",
+                    className="dropdown",
+                    min=2,
+                    max=300,
+                    step=1,
+                    value=100,
+                ),
+            ],),
+        ], className="menu"),
         html.Div(
-        dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": np.arange(1,data["temp"].count()),
-                        "y": data["humidity"],
-                        "type": "lines",
-                    },
-                ],
-                "layout": {"title": "Humidity over Time"},
-            },
-        ),className="card"),
-    ], className = "wrapper",
+            children=dcc.Graph(id="main-display", figure={}),
+            className="card"),
+    ],
+    className="wrapper",
 )
 
+
+@app.callback(
+    [Output("test_stuff", "children"), Output("main-display", "figure")],
+    [
+        Input("type-filter", "value"),
+        Input("records","drag_value")
+    ],
+)
+def update_graph(data_type,records):
+    # dff = data.copy()
+    main_display_figure = px.line(data, x=np.arange(
+        1, data[data_type][:records].count()+1), y=data[data_type][:records], title=data_type)
+
+    dash_update = [f"The Dash is now showing the {data_type} Readings"]
+    return dash_update, main_display_figure
 
 
 if __name__ == "__main__":
